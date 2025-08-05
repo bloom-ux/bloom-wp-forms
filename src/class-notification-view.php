3b32ac6dbf0722f77b4a4b585665da357d66530f
Basilio Cáceres <155106+basilio@users.noreply.github.com>
2025-08-05 19:24:48 -0400

    se escapan strings

--- src/class-notification-view.php
+++ src/class-notification-view.php
@@ -26,9 +26,9 @@ class Notification_View extends Form_View {
 	 */
 	public function __toString() {
 		$out      = '<div class="container" style="font-size:1rem;line-height:1.25;">';
-		$out     .= '<h1 style="font-weight:700;margin:1rem 0;font-size:1.75rem;">' . $this->form->get_property( 'title' ) . '</h1>';
+		$out     .= '<h1 style="font-weight:700;margin:1rem 0;font-size:1.75rem;">' . esc_html( $this->form->get_property( 'title' ) ) . '</h1>';
 		$out     .= '<div class="description" style="text-transform:uppercase;margin:1rem 0 1.5rem;opacity:.75;font-weight:500">';
-			$out .= '#' . $this->form->get_property( 'entry_id' ) . ' / ' . wp_date( 'd F Y - H:i:s', $this->form->get_property( 'submitted_date' )->format( 'U' ), false );
+			$out .= '#' . esc_html( $this->form->get_property( 'entry_id' ) ) . ' / ' . esc_html( wp_date( 'd F Y - H:i:s', $this->form->get_property( 'submitted_date' )->format( 'U' ), false ) );
 		$out     .= '</div>';
 		$out     .= apply_filters( 'bloom_forms_form_notification_pre_fields', '', $this, $this->form );
 		foreach ( $this->form->get_children() as $element ) {
@@ -46,20 +46,20 @@ class Notification_View extends Form_View {
 				$out .= $element;
 			} else {
 				if ( is_callable( array( $element, 'get_label' ) ) && ! empty( $element->get_label() ) ) {
-					$out .= '<div><b style="font-weight:500">' . $element->get_label() . '</b></div>';
+					$out .= '<div><b style="font-weight:500">' . esc_html( $element->get_label() ) . '</b></div>';
 				}
 				if ( is_callable( array( $element, 'get_value' ) ) ) {
 					if ( $element instanceof Textarea ) {
-						$out .= nl2br( $value );
+						$out .= nl2br( esc_html( $value ) );
 					} elseif ( $element instanceof Input && $element->get_attribute( 'type' ) === 'date' ) {
 						// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r
 						$value = is_string( $element->get_value() ) ? $element->get_value() : '<pre>' . print_r( (array) $element->get_value(), true ) . '</pre>';
 						$value = wp_date( 'd F Y', strtotime( $value . ' 12:00:00' ) );
-						$out  .= '<div>' . $value . '</div>';
+						$out  .= '<div>' . esc_html( $value ) . '</div>';
 					} else {
 						// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r
 						$value = is_string( $element->get_value() ) ? $element->get_value() : '<pre>' . print_r( (array) $element->get_value(), true ) . '</pre>';
-						$out  .= '<div>' . $value . '</div>';
+						$out  .= '<div>' . esc_html( $value ) . '</div>';
 					}
 				}
 			}
